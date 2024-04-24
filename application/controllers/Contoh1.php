7f5a5914b6eec73d7b81e188669abca620f45c38
yoshuanm <yoshuamanalu28@gmail.com>
2024-04-24 20:07:47 +0700

    upload tugas pertemuan 1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>"
+        echo"nama saya Yoshua Nicolas Manalu
+        Saya Tinggal di bekasi saya orangnya ganteng";
+
+    }
+}
+?>
