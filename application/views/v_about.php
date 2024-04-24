4b4f9920083f36822989cf891dc126d3aa90fcec
yoshuanm <yoshuamanalu28@gmail.com>
2024-04-24 20:22:15 +0700

    Upload pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,28 @@
+<section> 
+<head>
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url() 
+?>assets/stylebuku.css">
+   
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+    <li>Nama Depan : Yoshua</li>
+    <li>Nama Belakang : Manalu</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+    <li> Bekasi</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+    <li>Tangerang</li>
+ </ul>
+ <h4>Role Favorite</h4>
+ <ul type="square">
+    <li>Jungler</li>
+    <li>Retribution Only</li>
+ </ul>
+</head>
+</section>
---
+++ application/views/v_header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+
+<head>
+    <meta charset ="utf=8">
+    <title> WEB PROG II | Merancang template dengan codeigniter</title>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/stylebuku.css">
+</head>
+
+<body>
+    <div id = "wrapper">
+        <header>
+            <hgroup>
+                <h1>rentalBuku.net</h1>
+                <h3>Membuat Template Sederhana</h3>
+            </hgroup>
+            <nav>
+                <ul>
+            <li><a href="<?php echo base_url(). "index.php/web" ?>">Home</a></li>
+            <li><a href="<?php echo base_url(). "index.php/web/about" ?>">About</a></li>
+                <ul>
+            </nav>
+            <div class="clear"></div>
+        </header>
